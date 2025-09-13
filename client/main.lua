ce01ec8f592885ad992ce0dfe49850fb69709cf6
NewwyKung <cnewwy@gmail.com>
2025-09-13 21:04:07 +0700

    Add texture replacement for minimap blips
    - Implemented a new client script to replace minimap blip textures using a runtime texture dictionary.
    - Added functionality to load and apply custom textures from the 'assets' directory.
    - Updated fxmanifest to include the new client script and asset files.

---
+++ client/main.lua
@@ -0,0 +1,21 @@
+local Setting = {
+    Replaces = {
+        ['blips_texturesheet_ng'] = 'assets/blips_texturesheet_ng.png'
+    }
+}
+
+CreateThread(function()
+    Wait(5000)
+    local dict_txd = CreateRuntimeTxd('replace_blip')
+    for name, path in pairs(Setting.Replaces) do 
+        local texture = CreateRuntimeTextureFromImage(dict_txd, 'replace_' .. name, path)
+    end
+    RequestStreamedTextureDict('replace_blip', false)
+    while not HasStreamedTextureDictLoaded('replace_blip') do 
+        Wait(100)
+        print('Loading Texture Dict...')
+    end
+    for name, _ in pairs(Setting.Replaces) do
+        AddReplaceTexture('minimap', name, 'replace_blip', 'replace_' .. name)
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game "gta5"
+author "Newwy"
+version '1.0.0'
+description ''
+lua54 'yes'
+
+client_scripts {
+    'client/main.lua',
+}
+
+files {
+    'assets/**.png',
+}
